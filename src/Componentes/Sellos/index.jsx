fc8d89634ad4ad7c813c911cdf4d35a7be9bb442
daniboyka <danielcaz@gmail.com>
2023-10-13 21:20:41 -0300

    css empiezo sellos

---
+++ src/Componentes/Sellos/index.jsx
@@ -0,0 +1,7 @@
+export const Sellos = () => {
+    return(
+        <div className="flex justify-center w-full h-64 m-2">
+            <h1>sello</h1>
+        </div>
+    )
+}
--- src/pague/Home/index.jsx
+++ src/pague/Home/index.jsx
@@ -1,15 +1,18 @@
 import { Carrusel } from "../../Componentes/CarruselComun/Carrusel";
-import bateria2 from "../../utiles/IMG/baterias/bateria_1.png";
-import bateria1 from "../../utiles/IMG/baterias/bateria_2.png";
+import { Sellos } from "../../Componentes/Sellos";
+import bateria2 from "../../utiles/IMG/baterias/bateria_1.jpg";
+import bateria1 from "../../utiles/IMG/baterias/bateria_2.jpg";
 import bateria3 from "../../utiles/IMG/baterias/bateria_3.jpg";
+import bateria4 from "../../utiles/IMG/baterias/bateria_4.jpg";
 
 export const Home = () => {
-  const images = [bateria1, bateria2, bateria3];
+  const images = [bateria1, bateria2, bateria3, bateria4];
 
   return (
     
-    <div className="bg-indigo-600 w-full h-screen">       
+    <div className="bg-indigo-600 w-full">       
       <Carrusel images={images} autoplay={true} showButtons={true} />
+      <Sellos/>
     </div>
     
   );

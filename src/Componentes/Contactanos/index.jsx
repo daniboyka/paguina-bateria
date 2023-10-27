154b29729154cd5a4337445217934f3de668a8fa
Daniel <danielcaz@gmail.com>
2023-10-27 20:02:08 -0300

    termine, creo.., el contactanos

--- src/Componentes/Contactanos/index.jsx
+++ src/Componentes/Contactanos/index.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { BateriatContext } from "../../Contex/";
 import fondo8 from "../../utiles/IMG/Fondos/fondo8.avif";
 import { XMarkIcon } from "@heroicons/react/24/solid";
-
 //Poruct Detail- Abrir y cerrar detail
 
 export const Contactanos = () => {
   const Context = useContext(BateriatContext);
+
+
 
   return (
     <aside
@@ -22,25 +23,24 @@ export const Contactanos = () => {
       }`}
     >
       <div className="h-full w-full relative flex flex-col items-center ">
-
-      <div
-        className="cursor-pointer absolute flex mt-3 mb-0"
-        onClick={() => Context.cerrarContactos()}
-      >
-        <XMarkIcon className="h-8 w-8  hover:border-2 border-spacing-9 rounded-full" />
-      </div>
-      <div className="absolute mt-44 flex flex-col gap-10">        
-        <div>
-          <p>Correo: damiancaseres@gmail.com</p>
+        <div
+          className="cursor-pointer absolute flex mt-3 mb-0"
+          onClick={() => Context.cerrarContactos()}
+        >
+          <XMarkIcon className="h-8 w-8  hover:border-2 border-spacing-9 rounded-full" />
         </div>
-        <div>
-          <p>Telefono: 3442-578816</p>
+        <div className="absolute h-40 w-full mt-32 sm:ml-40 flex flex-col items-center sm:items-start gap-10">
+          <div className="">
+            <h2 className="text-lg sm:text-5xl font-sans subpixel-antialiased font-semibold text-teal-600 pt-1">
+              Contactate con nosotros!
+            </h2>
+          </div>
+          <div className="">
+            <p className="text-base sm:text-xl font-serif subpixel-antialiased"><strong>Correo:</strong><a href="mailto:caseresdami@gmail.com.ar"> caseresdami@gmail.com.ar</a></p><br></br>
+            <p className="text-base sm:text-xl font-serif subpixel-antialiased lining-nums"><strong>Telefono:</strong>  <a href="tel:3442578816"> 3442-578816</a></p><br></br>
+            <p className="text-base sm:text-xl font-serif subpixel-antialiased lining-nums"><strong>Telefono 2:</strong><a href="tel:3442578817"> 3442-578817</a></p>
+          </div>
         </div>
-        <div>
-          <p>Telefono 2: 3442-578816</p>
-        </div>
-      </div>
-
       </div>
     </aside>
   );
